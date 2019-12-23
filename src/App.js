507f4335063c198e19f1038414c5196014db2051
Analista 2 <analista2@inoveagora.local>
2019-12-23 16:47:22 -0300

    Comit componentes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import NavComp from './components/NavBar';
+import CardProducts from './components/CardsProducts';
+import {Row, Jumbotron, Container} from 'react-bootstrap';
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <NavComp/>
+          <Container>
+            <Jumbotron className="m-auto">
+              <Row>
+                <CardProducts/>
+              </Row>
+            </Jumbotron>
+          </Container>
+
+        
+        
+        
       </header>
     </div>
   );
